88749dbc495249523aed938f6efc92cb066c6a8d
oziohana5-jpg <---GitHub>
2026-09-14 20:14:12 +0300

    Extend emoji confetti to full screen

--- src/components/EmojiConfetti.tsx
+++ src/components/EmojiConfetti.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useTheme } from '../contexts/ThemeContext';
 
-const EMOJIS = ['✨', '💿', '🎵', '💜', '⚡', '🌈', '🎧', '💫', '🎶', '⭐'];
+const EMOJIS = ['✨', '💿', '🎵', '💜', '⚡', '🌈', '🎧', '💫', '🎶', '⭐', '🎼', '🎛️', '🎚️', '🔊', '🎤', '🪩', '📀'];
 
 interface ConfettiPiece {
   id: number;
